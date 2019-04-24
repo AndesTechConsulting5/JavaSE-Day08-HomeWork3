d8ac3d2b56298464eb88271bdd2dbe843906046d
a0z9 <a0z9@rambler.ru>
2019-04-24 11:03:22 +0300

    Homework3 init

---
+++ src/main/java/org/andestech/learning/rfb19/g4/homework3/AppHome.java
@@ -0,0 +1,15 @@
+// rename package
+package org.andestech.learning.rfb19.g4.homework3;
+
+/**
+ * Home work 3
+ *
+ */
+public class AppHome
+{
+    public static void main( String[] args )
+    {
+
+
+    }
+}
---
+++ src/test/java/org/andestech/learning/rfb19/g4/homework3/AppHomeTest.java
@@ -0,0 +1,20 @@
+package org.andestech.learning.rfb19.g4.homework3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple AppHome.
+ */
+public class AppHomeTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
