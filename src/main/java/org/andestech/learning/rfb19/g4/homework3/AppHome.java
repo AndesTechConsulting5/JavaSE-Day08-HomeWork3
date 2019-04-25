2194e01d7618b85036afd7177f80ba96d80b3f22
Vladimir <ovodenko@gmail.com>
2019-04-25 14:25:34 +0300

    первая итерация

--- src/main/java/org/andestech/learning/rfb19/g4/homework3/AppHome.java
+++ src/main/java/org/andestech/learning/rfb19/g4/homework3/AppHome.java
@@ -1,14 +1,31 @@
 // rename package
 package org.andestech.learning.rfb19.g4.homework3;
 
+
 /**
  * Home work 3
  *
  */
 public class AppHome
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws DebitAccountException {
+
+        Customer cus1 = new Customer("Vladimir", "Ovod", 19, "M", true );
+
+
+        try { DebitAccount  dbAcc_1 = new DebitAccount(1, 40, cus1 );
+        }
+        catch (DebitAccountException e) {
+            System.out.println("----");
+            System.out.println( e.getMessage() );
+        };
+
+
+        CreditAccount crAcc_1 = CreditAccount.create(2, 10000, cus1 );
+
+        crAcc_1.withdrawal( 100);
+        crAcc_1.putMoney( 2000000);
+
 
 
     }
---
+++ src/main/java/org/andestech/learning/rfb19/g4/homework3/CreditAccountException.java
@@ -0,0 +1,11 @@
+package org.andestech.learning.rfb19.g4.homework3;
+
+public class CreditAccountException extends RuntimeException {
+
+    public CreditAccountException(String m)
+    {
+        super(m);
+    }
+
+
+}
